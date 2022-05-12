9d84650e5b5bb79efb83158f844f1751446249ba
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-05-12 10:24:54 +0700

    ntkhanh-preparing basic environment and source 2

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const morgan = require('morgan')
+const ehbs = require('express-handlebars')
+const path = require('path')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+// view engine set up
+app.set('views', './views')
+app.set('view engine', 'hbs')
+require('express-handlebars-sections')(ehbs)
+app.engine('.hbs', ehbs.engine({
+  extname: '.hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan("dev"))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', {layout: false});
 })
 
 app.listen(port, () => {
